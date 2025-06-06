75ce758f0f62dbd335ca9f89a1653d7b7e0a68f1
cfontour <cfontour@gmail.com>
2025-06-06 10:13:54 -0300

    Add files via upload

---
+++ descarga.js
@@ -0,0 +1,8 @@
+// descarga.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("descarga.js cargado correctamente");
+
+    // Podés agregar aquí funcionalidades futuras si querés ocultar o mostrar cosas,
+    // validar si el dispositivo es Android, etc.
+});

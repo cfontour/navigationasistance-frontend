74b5ae90ed52a0d6b72c23300df6a4f38c8c03a0
cfontour <cfontour@gmail.com>
2025-08-20 09:37:12 -0300

    Add files via upload

--- maparepeWeatherAPI.js
+++ maparepeWeatherAPI.js
@@ -1192,7 +1192,8 @@ async function cargarEmbarcacionesAIS() {
         console.log(`🚢 Cargando embarcaciones AIS para área: ${north},${south},${east},${west}`);
 
         // AISHub API endpoint
-        const url = `http://data.aishub.net/ws.php?username=demo&format=1&output=json&compress=0&latmin=${south}&latmax=${north}&lonmin=${west}&lonmax=${east}`;
+        //const url = `http://data.aishub.net/ws.php?username=demo&format=1&output=json&compress=0&latmin=${south}&latmax=${north}&lonmin=${west}&lonmax=${east}`;
+        const url = `https://data.aishub.net/ws.php?username=demo&format=1&output=json&compress=0&latmin=${south}&latmax=${north}&lonmin=${west}&lonmax=${east}&timespan=10`;
 
         const response = await fetch(url);
 

53b8c5b9954403e7b53cd4a670b17cae3e17b435
cfontour <cfontour@gmail.com>
2025-11-17 15:45:19 -0300

    Add files via upload

--- regatasparticipantes.js
+++ regatasparticipantes.js
@@ -16,6 +16,7 @@ async function obtenerRutaIdPorNombre(nombreRuta = "PUNTA DEL ESTE") {
 }
 
 async function cargarUsuarios() {
+  // grupo regatas
   const res = await fetch("https://navigationasistance-backend-1.onrender.com/usuarios/listarGrupos/regatas");
   usuarios = await res.json();
 

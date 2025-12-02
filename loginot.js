a54b09ccef718f856b86382f2e562ae021c61dfb
cfontour <cfontour@gmail.com>
2025-12-02 09:51:13 -0300

    Add files via upload

---
+++ loginot.js
@@ -0,0 +1,33 @@
+async function login() {
+    const usuario = document.getElementById("usuario").value;
+    const password = document.getElementById("password").value;
+
+    if (!usuario || !password) {
+        alert("Debe ingresar usuario y contraseña");
+        return;
+    }
+
+    try {
+        const backendUrl = "https://navigationasistance-backend-1.onrender.com";
+
+        const res = await fetch(`${backendUrl}/usuarios/login/${usuario}/${password}`);
+
+        if (res.status === 200) {
+            const usuarioData = await res.json(); // viene el JSON del usuario
+
+            // 🔹 Guardamos "sesión" sencilla en el navegador
+            localStorage.setItem("usuarioLogueado", usuarioData.id); // o usuarioData.usuario, etc.
+
+
+            // Login correcto
+            window.location.href = "menuop.html";
+        } else {
+            // No autorizado
+            window.location.href = "noacceso.html";
+        }
+
+    } catch (e) {
+        console.error("Error en login:", e);
+        alert("Error de conexión con el servidor");
+    }
+}
